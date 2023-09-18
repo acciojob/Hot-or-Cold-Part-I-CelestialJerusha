e28b011bb2ba291620ea0065485b02c877fa3a34
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-09-18 21:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+let x = Math.floor((Math.random() * 10) + 1);
+let min;
+function submit(){
+let n = document.getElementById("guess").value;
+	if(n!==x){
+		min = x-n;
+		if(n < min){
+		document.getElementById("respond").innerHTML = "Getting hotter and guess higher";
+		}
+		else if(n > min){
+			document.getElementById("respond").innerHTML = "Getting hotter and guess lower";
+		}
+		else{
+		document.getElementById("respond").innerHTML = "guess is right";
+		}
+	}
+	else{
+		document.getElementById("respond").innerHTML = "guess is right";
+		}
+	
+	
 
+}
